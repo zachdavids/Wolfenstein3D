faba5a70cc92db15b140b95762989df5ffcbe8da
Zach Davids <zach@zachdavids.com>
2019-03-20 19:50:33 -0400

    Refactored Attenuation and PointLight

--- Wolfenstein/Attenuation.h
+++ Wolfenstein/Attenuation.h
@@ -1,23 +1,7 @@
 #pragma once
 
-class Attenuation
+struct Attenuation
 {
-public:
-
-	Attenuation() {};
-	Attenuation(float constant, float linear, float exponent);
-
-	float GetConstant() { return constant_; };
-	void SetConstant(float constant) { constant_ = constant; };
-
-	float GetLinear() { return linear_; };
-	void SetLinear(float linear) { linear_ = linear; };
-
-	float GetExponent() { return exponent_; };
-	void SetExponent(float exponent) { exponent_ = exponent; };
-
-private:
-
 	float constant_;
 	float linear_;
 	float exponent_;
--- Wolfenstein/PointLight.cpp
+++
@@ -1,8 +0,0 @@
-#include "PointLight.h"
-
-PointLight::PointLight(BaseLight* base_light, Attenuation* attenuation, glm::vec3 position)
-{
-	base_light_ = base_light;
-	attenuation_ = attenuation;
-	position_ = position;
-}
